2470dbc3a5a0372d53e0891202a150fc3b371b8a
lasr21 <lasr21@gmail.com>
2014-11-05 21:16:49 -0600

    2 png pantalla completa y una recortada

--- screen.js
+++ screen.js
@@ -5,26 +5,20 @@ var date = new Date()
 var nom = date.getFullYear() + '_' + date.getMonth() + '_' + date.getDate()
 
 //Definimos el tamaño de la pantalla
-page.viewportSize = {
-  width: 2048,
-  height: 900
-};
+page.viewportSize = { width: 2048, height: 900 };
 
-//Para que solgo saque la imagen del tamaño correcto
-page.clipRect = {
-  top: 260,
-  left: 545,
-  width: 970,
-  height: 600,
-};
-
-//funcion para que espere 3 segundos para que cargue por completo la pagina
-var tomascreen = function () {
-  page.render(nom + '_mex_.png');
+var tomascreen_recortado = function () {
+  page.clipRect = { top: 260, left: 545, width: 970, height: 600, };
+  page.render('rec_' + nom + '_mex_.png');
   phantom.exit();	
+};
+
+var tomascreen_completo = function () {
+  page.render('com_' + nom + '_mex_.png');
 };
 
 //Mexico screenshot
 page.open("https://location.services.mozilla.com/map#5/22.634/-101.777", function() {
-  setTimeout(tomascreen, 3000);
+  setTimeout(tomascreen_completo, 3000);
+  setTimeout(tomascreen_recortado, 3000);
 });
